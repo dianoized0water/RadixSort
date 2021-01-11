aebd8cc63c0bd81c20702509c4ca9b7a0d2c18e0
diana_yang <dyang20@stuy.edu>
2021-01-11 13:55:43 -0500

    added nth and length

--- Radix.java
+++ Radix.java
@@ -1,4 +1,4 @@
-import.java.util.*
+import java.util.*;
 
 public class Radix{
   public static void main(String[] args){
@@ -6,14 +6,17 @@ public class Radix{
   }
 
   public static int nth(int n, int col){
-
+    double a = col * 1.0;
+    int b = (int)(n/(Math.pow(10.0, a)));
+    return (int) b%10;
   }
 
   public static int length(int n){
-
+    double a = 1.0 * Math.abs(n);
+    return (int)(Math.log10(a)+ 1);
   }
 
   public static void merge(MyLinkedList original, MyLinkedList[] buckets){
-    
+
   }
 }
